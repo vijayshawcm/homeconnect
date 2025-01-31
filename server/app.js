c1669f1cdb721f52e501e75bb104eff69752b57d
Isaac Lee <isaaclee.du@gmail.com>
2025-01-31 16:22:26 +0800

    Restructured MongoDB connection logic. Created base user registration logic. Sample registration form created.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
-// Load environment variables
-dotenv.config();
+// Multer used for form handling but idk if i wanna keep it cuz html forms use urlencoding
+const multer = require('multer');
+const mongodb = require('./mongodb');
+const User = require('./models/user');
 
 // Initialize Express app
 const app = express();
@@ -11,11 +11,52 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+const upload = multer();
 
-// Test route
+// Set default view engine and file extension to render
+app.set('view engine', 'ejs');
+
+// Home page
 app.get('/', (req, res) => {
 	res.send('HomeConnect Backend is running!');
 });
+
+// Very pretty register form
+// TODO: Link this with front end form lol
+app.get('/register', (req, res) => {
+	res.render('./register');
+});
+
+// Register user
+app.post('/register-user', upload.none(), async (req, res) => {
+	console.log(req.body);
+
+	try {
+		const user = await User.create({
+			username: req.body.username,
+			password: req.body.password,
+		})
+		
+		return res.status(200).json(user);
+	} catch (err) {
+		return res.status(500).send("Internal Server Error.")
+	}
+});
+
+
+
+// app.get('/v', async (req, res) => {
+// 	const collections = await mongodb.db.listCollections().toArray()
+// 	console.log('Collections:', collections);
+
+// 	console.log('sending stuff now');
+// 	res.json(collections);
+// });
+
+
+
+
 
 // Export the app
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,13 @@
+const dotenv = require('dotenv');
+// Load environment variables
+dotenv.config();
+
 const app = require('./app');
-const mongoose = require('mongoose');
 
 // Environment variables
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
 
-// Connect to MongoDB
-mongoose
-	.connect(MONGODB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log('Connected to MongoDB');
-		// Start the server
-		app.listen(PORT, () => {
-			console.log(`Server is running on port ${PORT}`);
-		});
-	})
-	.catch((err) => {
-		console.error('Error connecting to MongoDB:', err);
-	});
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+})
