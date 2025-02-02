b1eb382cdbfdf8ca4274c514db8f977bf6df893e
Vijay Shaw <vijayshawcm@gmail.com>
2025-02-02 23:22:11 +0800

    Update routes to include Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
-export default function App() {
-	return <Login />;
+export default function AppRoutes() {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Login />} /> {/* Default route */}
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</Router>
+	);
 }
