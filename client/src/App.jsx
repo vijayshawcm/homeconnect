68492475fa3ac62e865b897a853ab171538caf5d
Vijay Shaw <vijayshawcm@gmail.com>
2025-02-03 11:49:18 +0800

    Update routes to include OTP Verification page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import PhoneVerification from './pages/auth/PhoneVerification';
+import OTPVerification from './pages/auth/OTPVerification';
 
 export default function AppRoutes() {
 	return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/phone-verification" element={<PhoneVerification />} />
+				<Route path="/otp-verification" element={<OTPVerification />} />
 			</Routes>
 		</Router>
 	);
