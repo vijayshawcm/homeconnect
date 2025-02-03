a3cef3be4d553d02626359e0eba0fc797ccc13cf
Vijay Shaw <vijayshawcm@gmail.com>
2025-02-03 10:51:49 +0800

    Update routes to include Phone verification page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import PhoneVerification from './pages/auth/PhoneVerification';
 
 export default function AppRoutes() {
 	return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
 				<Route path="/" element={<Login />} /> {/* Default route */}
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/phone-verification" element={<PhoneVerification />} />
 			</Routes>
 		</Router>
 	);
