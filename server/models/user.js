f2b5025d36c2aa57c781d0a3381c97b9a241fe11
LazyZeny <isaaclee.du@gmail.com>
2025-02-06 12:29:48 +0800

    Delete server/models/user.js

--- server/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-var userSchema = new mongoose.Schema({
-    username: String,
-    passwordHash: String,
-})
-
-module.exports = mongoose.model('User', userSchema)
