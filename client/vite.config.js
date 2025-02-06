a4b304837e459d1627d2c3dfe6cb24a6fd4b5dbf
Vijay Shaw <vijayshawcm@gmail.com>
2025-02-06 15:35:34 +0800

    shadcn/ui initialization

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [react()],
