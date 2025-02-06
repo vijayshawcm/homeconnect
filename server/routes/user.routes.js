7c01dbd02e53b9f01c426d89e3cfeb1323923c21
Lim Kien Aik <kienaik0903@gmail.com>
2025-02-06 22:38:06 +0800

    move everything to their own controllers and routes.

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,19 @@
 const express = require("express");
+// Multer used for form handling but idk if i wanna keep it cuz html forms use urlencoding
+const multer = require("multer");
+const upload = multer();
 
-const { createUser } = require("../controllers/user.controller");
+const {
+  secretPage,
+  registerUser,
+  loginUser,
+  logoutUser
+} = require("../controllers/user.controller");
 
 const router = express.Router();
-router.post("/", createUser);
+router.get("/secret", secretPage);
+router.get('/logout', logoutUser)
+router.post("/register", upload.none(), registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
