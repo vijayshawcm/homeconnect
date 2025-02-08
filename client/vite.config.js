be74262b78e6644314931152e9c82b3be1094ad2
Lim Kien Aik <kienaik0903@gmail.com>
2025-02-08 12:24:24 +0800

    Added server proxy for easier endpoint calling

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
 
 export default defineConfig({
-	plugins: [react()],
-	css: {
-		postcss: {
-			plugins: [tailwindcss(), autoprefixer()],
-		},
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/server": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
