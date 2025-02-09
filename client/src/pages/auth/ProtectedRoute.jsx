f1cc9f224cc42fa72107854a271ffa26ea4ecdb2
Isaac Lee <isaaclee.du@gmail.com>
2025-02-09 16:59:11 +0800

    Route that redirects users to login page if they are not signed in

---
+++ client/src/pages/auth/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+// Route to reroute users to login page if they arent authenticated.
+import { Navigate, Outlet } from "react-router-dom";
+import { userAuthStore } from "@/store/userAuth";
+
+export default function ProtectedRoute() {
+    console.log("protected route hi")
+    const {
+        isAuthenticated,
+    } = userAuthStore();
+
+    if(isAuthenticated){
+        return <Outlet /> // Route user to the page they are trying to access if they are authenticated
+    } else {
+        return <Navigate to = '/login' /> //Re-route user to login page if they are not authenticated
+    }
+}
