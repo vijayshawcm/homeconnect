0bfb97a203d841afb0776a0d1d7cd8658641d8b1
Vijay Shaw <vijayshawcm@gmail.com>
2025-02-09 21:40:23 +0800

    delegate routing to AppRoutes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import PhoneVerification from "./pages/auth/PhoneVerification";
-import OTPVerification from "./pages/auth/OTPVerification";
-import AppliancePage from "./pages/home/AppliancePage";
+import React from 'react';
+import AppRoutes from './routes';
 
-export default function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} /> {/* Default route */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/phone-verification" element={<PhoneVerification />} />
-        <Route path="/otp-verification" element={<OTPVerification />} />
-        <Route path="/appliancePage" element={<AppliancePage />} />
-      </Routes>
-    </Router>
-  );
+function App() {
+	return (
+		<div className="min-h-screen">
+			<AppRoutes />
+		</div>
+	);
 }
+
+export default App;
