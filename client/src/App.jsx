0111bac100f7a60c8e918968aab350f28587a48e
Isaac Lee <isaaclee.du@gmail.com>
2025-02-09 16:58:50 +0800

    Added protected route and dashboard redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import PhoneVerification from "./pages/auth/PhoneVerification";
 import OTPVerification from "./pages/auth/OTPVerification";
+import ProtectedRoute  from "./pages/auth/ProtectedRoute";
 import AppliancePage from "./pages/home/AppliancePage";
+import Dashboard from "./pages/home/Dashboard";
 
 export default function AppRoutes() {
   return (
@@ -15,6 +17,10 @@ export default function AppRoutes() {
         <Route path="/phone-verification" element={<PhoneVerification />} />
         <Route path="/otp-verification" element={<OTPVerification />} />
         <Route path="/appliancePage" element={<AppliancePage />} />
+        {/* Put protected routes in here */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </Router>
   );
