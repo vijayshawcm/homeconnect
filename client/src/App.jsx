dfd917c2c5efdddf105d7aeb1cb470e7e7881101
Lim Kien Aik <kienaik0903@gmail.com>
2025-02-10 15:35:54 +0800

    updated app and package.sjon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import PhoneVerification from "./pages/auth/PhoneVerification";
-import OTPVerification from "./pages/auth/OTPVerification";
-import AppliancePage from "./pages/home/AppliancePage";
-import { useHomeStore } from "./store/home";
-import { useEffect } from "react";
-import { useRoomStore } from "./store/room";
-import Sidebar from "./components/home/Sidebar";
-
-export default function AppRoutes() {
-  const { homes, fetchHomes } = useHomeStore();
-  const { rooms, fetchRoomsByHome } = useRoomStore();
-
-  useEffect(() => {
-    fetchHomes();
-  }, [fetchHomes]);
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} /> {/* Default route */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/phone-verification" element={<PhoneVerification />} />
-        <Route path="/otp-verification" element={<OTPVerification />} />
-        {homes.map((home) => {
-          console.log(home.name);
-        })}
-      </Routes>
-    </Router>
-  );
-=======
 import React from 'react';
 import AppRoutes from './routes';
 
@@ -42,7 +7,6 @@ function App() {
 			<AppRoutes />
 		</div>
 	);
->>>>>>> main
 }
 
 export default App;
