1ed1ee17de39251cf1dcd5b0028cbba29bcaac9f
Lim Kien Aik <kienaik0903@gmail.com>
2025-02-12 12:06:22 +0800

    Deleted unused directories

---
+++ client/src/components/home/appSidebar.jsx
@@ -0,0 +1,19 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+const appSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader>LOGO</SidebarHeader>
+      <SidebarContent></SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default appSidebar;
--- client/src/pages/home/Dashboard.jsx
+++ client/src/pages/home/Dashboard.jsx
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-const Dashboard = ( { home }) => {
-  return (
-    <div>Dashboard</div>
-  )
-}
-
-export default Dashboard
-=======
 export default function Dashboard() {
     console.log("dashboard hi");
     return (
         <h1> Welcome to HomeConnect! </h1>
     )
 }
->>>>>>> main
