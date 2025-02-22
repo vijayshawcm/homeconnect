9a3a7fe3af0bdf5fb0c0acfb83d8da31556a489f
Vijay Shaw <vijayshawcm@gmail.com>
2025-02-22 18:42:35 +0800

    add title utility function

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,13 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
+
+// Title utility function
+export const updatePageTitle = (title) => {
+	document.title = title
+		? `HomeConnect | ${title}`
+		: 'HomeConnect | Smart Home Management';
+};
