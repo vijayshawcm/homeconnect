55ad4cc62b98ce54b788878b2d2aa3b7e92e9a9d
Lim Kien Aik <kienaik0903@gmail.com>
2025-02-24 13:57:45 +0800

    solve merge conflict in routes.jsx

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD:client/src/routes.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthContainer from "./auth/AuthContainer"; // Import AuthContainer
-import ProtectedRoute from "./pages/auth/ProtectedRoute";
-import Dashboard from "./home/Dashboard";
-import NotFound from "./pages/NotFound";
-
-function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/login" element={<AuthContainer mode="login" />} />
-        <Route path="/register" element={<AuthContainer mode="register" />} />
-        <Route
-          path="/forgot-password"
-          element={<AuthContainer mode="forgot-password" />}
-        />
-        <Route path="/verify" element={<AuthContainer mode="verify" />} />
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        {/* 404 Route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '@/features/landing/LandingPage';
@@ -55,7 +26,6 @@ function AppRoutes() {
 			</Routes>
 		</Router>
 	);
->>>>>>> main:client/src/routes/routes.jsx
 }
 
 export default AppRoutes;
