706a5f95af57dd808b9ca1dbfd4fdd5d11b17d37
Lim Kien Aik <kienaik0903@gmail.com>
2025-03-07 05:26:06 +0800

    cleaned up imports

--- client/src/features/header/Header.jsx
+++ client/src/features/header/Header.jsx
@@ -1,12 +1,10 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { HeaderInput } from "./components/HeaderInput";
-import HeaderAvatar from "./components/HeaderAvatar";
 import DashboardHeader from "./components/DashboardHeader";
 import RoomsHeader from "./components/RoomsHeader";
 
 const Header = ({ mode }) => {
   return (
-    <div className="flex py-6 px-4 bg-[#FFFFFF]">
+    <div className="flex py-6 px-4 bg-[#FFFFFF] h-24">
       <div className="flex items-center gap-2 w-full">
         <SidebarTrigger />
         {mode === "dashboard" ? (
