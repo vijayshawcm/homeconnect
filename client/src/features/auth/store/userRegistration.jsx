68dd21092ca502e4aa7e8762018677bd6137a528
Vijay Shaw <vijayshawcm@gmail.com>
2025-03-06 21:53:14 +0800

    add fullName state and setFullName function to userRegistration store

--- client/src/features/auth/store/userRegistration.jsx
+++ client/src/features/auth/store/userRegistration.jsx
@@ -1,12 +1,14 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
-export const userRegistrationStore = create((set)=>({
-  username: null,
-  setUsername: (name) => set({ username: name }),
-  email: null,
-  setEmail: (email) => set({ email: email }),
-  phoneNum: null, // ? Do we need phone num?
-  setPhoneNum: (num) => set({ phoneNum: num }),
-  password: null,
-  setPassword: (pw) => set({ password: pw }),
-}))
+export const userRegistrationStore = create((set) => ({
+	username: null,
+	setUsername: (username) => set({ username }),
+	fullName: null,
+	setFullName: (fullName) => set({ fullName }),
+	email: null,
+	setEmail: (email) => set({ email }),
+	phoneNum: null, // ? Do we need phone num? // re: leave it for now ig
+	setPhoneNum: (phone) => set({ phone }),
+	password: null,
+	setPassword: (password) => set({ password }),
+}));
