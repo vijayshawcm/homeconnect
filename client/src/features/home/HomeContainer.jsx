8fc0e5f07af850a5a830e15596c0e508d0ae65ee
Lim Kien Aik <kienaik0903@gmail.com>
2025-03-14 00:00:23 +0800

    Added smooth transition div (Similiar to AuthContainer) to Home Container, for smooth fade in-out

--- client/src/features/home/HomeContainer.jsx
+++ client/src/features/home/HomeContainer.jsx
@@ -7,27 +7,41 @@ import Header from "../header/Header";
 import { useHomeStore } from "@/store/home";
 
 const HomeContainer = ({ mode }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+  const [isTransitioning, setIsTransitioning] = useState(false); // Track transition state
   const [currentMode, setCurrentMode] = useState(mode); // Track current mode
-  const {isLoading} = useHomeStore()
+  const { isLoading } = useHomeStore();
 
   useEffect(() => {
-    setCurrentMode(mode);
-    
-  }, [setCurrentMode, mode]);
+    // Prevent transition on first render, but allow after mount
+    if (!hasMounted) {
+      setCurrentMode(mode);
+      setHasMounted(true);
+      return;
+    }
+    setIsTransitioning(true); // Start transition
+    setTimeout(() => {
+      setCurrentMode(mode); // Update mode after transition
+      setIsTransitioning(false); // End transition
+    }, 150); // Match with transition duration
+  }, [hasMounted, setHasMounted, setIsTransitioning, setCurrentMode, mode]);
 
-  if (!isLoading) {
-    return <div>Loading...</div>;
-  }
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="bg-[#EFEFEF] overflow-hidden w-full flex flex-col">
-        <Header mode={currentMode}/>
-        {currentMode === "dashboard" ? (
-          <Dashboard />
-        ) : currentMode === "room" ? (
-          <RoomPage />
-        ) : null}
+      <main className="bg-[#EFEFEF] overflow-hidden w-full flex flex-1 flex-col">
+        <Header mode={currentMode} />
+        <div
+          className={`transition-opacity duration-300 flex flex-col flex-1 ${
+            isTransitioning ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          {currentMode === "dashboard" ? (
+            <Dashboard />
+          ) : currentMode === "room" ? (
+            <RoomPage />
+          ) : null}
+        </div>
       </main>
     </SidebarProvider>
   );
