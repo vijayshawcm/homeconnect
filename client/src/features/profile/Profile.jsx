1877389306b829756e8141c3d8f574bb5db0e117
Vijay Shaw <vijayshawcm@gmail.com>
2025-03-13 17:24:03 +0800

    update profile to include connected devices

--- client/src/features/profile/Profile.jsx
+++ client/src/features/profile/Profile.jsx
@@ -3,6 +3,7 @@ import { updatePageTitle } from '@/lib/utils';
 import ProfileHeader from '@/features/profile/components/ProfileHeader';
 import ProfileStats from '@/features/profile/components/ProfileStats';
 import RecentActivity from '@/features/profile/components/RecentActivity';
+import ConnectedDevices from '@/features/profile/components/ConnectedDevices';
 
 function Profile() {
 	useEffect(() => {
@@ -16,13 +17,17 @@ function Profile() {
 		email: 'test@gmail.com',
 		bio: 'Smart home enthusiast and tech lover',
 		avatarUrl: '',
+		joinDate: 'March 2025',
 	};
 
 	return (
-		<div className="container py-6 max-w-5xl">
+		<div className="container py-8 max-w-6xl mx-auto px-4 sm:px-6">
 			<ProfileHeader user={user} />
 			<ProfileStats />
-			<RecentActivity />
+			<div className="grid gap-8 md:grid-cols-2">
+				<RecentActivity />
+				<ConnectedDevices />
+			</div>
 		</div>
 	);
 }
