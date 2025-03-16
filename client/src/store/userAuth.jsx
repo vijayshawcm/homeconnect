c51b5f2deb55bb29d545632b09d3bb8ffb00c0ea
Isaac Lee <isaaclee.du@gmail.com>
2025-03-16 21:51:36 +0800

    User logout linked to zustand properly

--- client/src/store/userAuth.jsx
+++ client/src/store/userAuth.jsx
@@ -21,7 +21,16 @@ export const userAuthStore = create((set) => ({
     }
   },
 
-  logoutUser: () => set({ user: null, isAuthenticated: false }),
+  logoutUser: async () => {
+    const response = await fetch("server/auth/logout", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+
+    set({ user: null, isAuthenticated: false });
+  }
+
 }));
 
 // Fetch user login on app load
