fb1bdcfb35fcf8a61195ddae0fa9112a0845f24c
Isaac Lee <isaaclee.du@gmail.com>
2025-03-19 00:21:24 +0800

    Added new user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 const {
   updateEmail,
-  updatePassword
+  updatePassword,
+  updateUsername,
+  updateDisplayName,
+  updateAccountStatus
 } = require("../controllers/user.controller");
 
 const router = express.Router();
 router.patch("/updateEmail", updateEmail);
 router.patch("/updatePassword", updatePassword);
+router.patch("/updateUsername", updateUsername);
+router.patch("/updateDisplayName", updateDisplayName);
+router.patch("/accountStatus", updateAccountStatus);
 
 module.exports = router;
