9d59a49674b00d539317437b4c46bdb8a50055ec
Isaac Lee <isaaclee.du@gmail.com>
2025-03-19 10:32:39 +0800

    Added new user management routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,6 +5,9 @@ const {
   updatePassword,
   updateUsername,
   updateDisplayName,
+  updateTheme,
+  updateNotificationChannel,
+  updateNotificationType,
   updateAccountStatus
 } = require("../controllers/user.controller");
 
@@ -13,6 +16,9 @@ router.patch("/updateEmail", updateEmail);
 router.patch("/updatePassword", updatePassword);
 router.patch("/updateUsername", updateUsername);
 router.patch("/updateDisplayName", updateDisplayName);
+router.patch("/updateTheme", updateTheme);
+router.patch("/updateNotificationChannel", updateNotificationChannel);
+router.patch("/updateNotificationType", updateNotificationType);
 router.patch("/accountStatus", updateAccountStatus);
 
 module.exports = router;
