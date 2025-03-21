3feb617a6a81e212b4e546414f5f1e42be7f4c3e
Isaac Lee <isaaclee.du@gmail.com>
2025-03-21 14:32:09 +0800

    Added endpoint for dweller permission management

--- server/controllers/permissions.controller.js
+++ server/controllers/permissions.controller.js
@@ -52,7 +52,7 @@ const acceptInvite = async (req, res) => {
         return res.status(500).json("Missing argument(s) in request body.");
     }
 
-    // Extract argumets
+    // Extract arguments
     const username = req.body.username;
     const invite = req.body.invite;
 
@@ -101,7 +101,61 @@ const acceptInvite = async (req, res) => {
     return res.status(200).json("Home joined successfully.");
 }
 
+// Modify user permissions
+const modifyPermissions = async (req, res) => {
+    // Check for arguments in request body
+    if(!req.body.username || !req.body.permissions || !req.body.home) {
+        return res.status(500).json("Missing argument(s) in request body.");
+    }
+
+    // Extract username
+    const username = req.body.username;
+
+    // Attempt to locate user to be added to home
+    const user = await User.findOne({ 'userInfo.usernameLower': username.toLowerCase() });
+    if(!user) {
+        return res.status(404).json("User not found, did user sign up fail?");
+    }
+
+    // Query database for home to get current user permissions
+    const home = await Home.findById(req.body.home);
+    if(!home) {
+        return res.status(404).json("Could not find home.");
+    }
+
+    // Get dweller index
+    const currentDweller = home.dwellers.findIndex(e => e.user.equals(user._id))
+    if(currentDweller == -1) {
+        return res.status(404).json("User is not part of this home.")
+    }
+
+    const currentPermissions = Object
+        .keys(home.dwellers[currentDweller].accessLevel) // Get all existing keys in accessLevel
+        .filter(e => home.dwellers[currentDweller].accessLevel[e]) // Filter so that only present keys (true) are included in currentPermissions.
+        || []; //
+
+    const unsetPermissions = currentPermissions
+        .filter(e => !req.body.permissions.includes(e)) // Get removed permissions with filter
+        /* Reduce entries to single object with path to relevant permission in database
+           For example: if current dweller index is 0, path will be dwellers.0.accessLevel.${permission here}: true
+        */
+        .reduce((acc, e) => Object.assign(acc, { [`dwellers.${currentDweller}.accessLevel.${e}`]: true }), {}); 
+
+    const setPermissions = req.body.permissions
+        .reduce((acc, e) => Object.assign(acc, { [`dwellers.${currentDweller}.accessLevel.${e}`]: true }), {}); // Same logic as above
+
+    // Set permissions
+    await home.updateOne({
+        $set: setPermissions,
+        $unset: unsetPermissions
+    })
+
+    return res.status(200).json("User permissions updated successfully.");
+} 
+
+// TODO: kick user from home
 module.exports = {
     createInvite,
-    acceptInvite
+    acceptInvite,
+    modifyPermissions
 }
--- server/routes/permissions.routes.js
+++ server/routes/permissions.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const {
     createInvite,
-    acceptInvite
+    acceptInvite,
+    modifyPermissions,
 } = require("../controllers/permissions.controller");
 
 const router = express.Router();
 router.patch("/invite", createInvite);
-router.patch("/join", acceptInvite)
+router.patch("/join", acceptInvite);
+router.patch("/dweller", modifyPermissions);
 
 module.exports = router;
