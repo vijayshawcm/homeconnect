41464021618318bf54fd1f680b4c36a530f3141b
Isaac Lee <isaaclee.du@gmail.com>
2025-03-21 23:08:59 +0800

    Added remove dweller route

--- server/routes/permissions.routes.js
+++ server/routes/permissions.routes.js
@@ -4,11 +4,13 @@ const {
     createInvite,
     acceptInvite,
     modifyPermissions,
+    removeDweller,
 } = require("../controllers/permissions.controller");
 
 const router = express.Router();
 router.patch("/invite", createInvite);
 router.patch("/join", acceptInvite);
 router.patch("/dweller", modifyPermissions);
+router.patch("/remove", removeDweller)
 
 module.exports = router;
