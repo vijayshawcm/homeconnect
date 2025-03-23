944a2890ad6786121fc8d9b1e2a68be27bed5efc
Isaac Lee <isaaclee.du@gmail.com>
2025-03-24 02:06:22 +0800

    Added modify room endpoint

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -4,13 +4,15 @@ const {
   createRoom,
   getRoomsByHome,
   getRoomById,
+  modifyRoom,
   deleteRoom,
 } = require("../controllers/room.controller");
 
 const router = express.Router();
 router.post("/", createRoom);
 router.get("/:id", getRoomsByHome);
-router.get("/roomId/:id", getRoomById)
+router.get("/roomId/:id", getRoomById);
+router.patch("/:id", modifyRoom);
 router.delete("/:id", deleteRoom);
 
 module.exports = router;
