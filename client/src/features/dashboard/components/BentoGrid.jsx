bfe1008443b0939d7c096a15625889a9b7876083
Vijay Shaw <vijayshawcm@gmail.com>
2025-03-24 23:46:15 +0800

    add suggestions card and update bento grid to include it

--- client/src/features/dashboard/components/BentoGrid.jsx
+++ client/src/features/dashboard/components/BentoGrid.jsx
@@ -1,22 +1,25 @@
-import ProfileCard from "./ProfileCard";
-import SummaryCard from "./SummaryCard";
-import WeatherCard from "./WeatherCard";
+import ProfileCard from './ProfileCard';
+import SummaryCard from './SummaryCard';
+import WeatherCard from './WeatherCard';
+import SuggestionsCard from './SuggestionsCard';
 
 const BentoGrid = () => {
-  return (
-    <div className="grid auto-cols-auto lg:auto-cols-[1fr] auto-rows-[1fr] gap-8 grid-template-area p-4 ">
-      <div className="bentoProfile flex">
-        <ProfileCard />
-      </div>
-      <div className="bentoWeather flex">
-        <WeatherCard />
-      </div>
-      <div className="bentoSummary flex">
-        <SummaryCard/>
-      </div>
-      <div className="bg-blue-800 bentoSuggestion flex"></div>
-    </div>
-  );
+	return (
+		<div className="grid auto-cols-auto lg:auto-cols-[1fr] auto-rows-[1fr] gap-8 grid-template-area p-4 ">
+			<div className="bentoProfile flex">
+				<ProfileCard />
+			</div>
+			<div className="bentoWeather flex">
+				<WeatherCard />
+			</div>
+			<div className="bentoSummary flex">
+				<SummaryCard />
+			</div>
+			<div className="bentoSuggestion flex">
+				<SuggestionsCard />
+			</div>
+		</div>
+	);
 };
 
 export default BentoGrid;
